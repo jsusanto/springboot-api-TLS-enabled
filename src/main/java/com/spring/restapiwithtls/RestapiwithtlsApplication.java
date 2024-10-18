3b84d212894875c630768d23c50067fadf97365b
Jeffry Susanto <jeffry.susanto@medibank.com.au>
2024-10-18 14:46:13 +1100

    Initial Springboot Empty Prototype project.

---
+++ src/main/java/com/spring/restapiwithtls/RestapiwithtlsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.restapiwithtls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiwithtlsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiwithtlsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/restapiwithtls/RestapiwithtlsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.restapiwithtls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiwithtlsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
